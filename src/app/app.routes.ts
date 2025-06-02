d5ed3356e0fae0eb1770be733574dd9e08fa41ae
bootcamp04 <bootcamp04@deloitte.com>
2025-06-02 16:05:06 -0300

    criando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { AppComponent } from './app.component';
+import { DashClienteComponent } from './pages/dash-cliente/dash-cliente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'cliente',
+        component:DashClienteComponent
+    }
+];
