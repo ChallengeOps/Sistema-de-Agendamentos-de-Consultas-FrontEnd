a433489633bc4269efd8aa00ceb0b86a08e65904
bootcamp04 <bootcamp04@deloitte.com>
2025-06-02 14:38:16 -0300

    criando components dashcliente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashClienteComponent } from './pages/dash-cliente/dash-cliente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'cliente',
+        component: DashClienteComponent
+    }
+];
---
+++ src/app/pages/dash-cliente/dash-cliente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+
+@Component({
+  selector: 'app-dash-cliente',
+  imports: [HeaderComponent],
+  templateUrl: './dash-cliente.component.html',
+  styleUrl: './dash-cliente.component.css'
+})
+export class DashClienteComponent {
+
+}
