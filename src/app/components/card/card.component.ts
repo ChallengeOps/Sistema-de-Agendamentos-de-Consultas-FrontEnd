e3fc7eb4bf0afb5f8cf1818a873f39ffc2b5b56e
bootcamp04 <bootcamp04@deloitte.com>
2025-06-02 15:23:13 -0300

    criando pagina de cliente dashboard

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Servico } from '../../model/servico';
 
 @Component({
   selector: 'app-card',
@@ -8,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class CardComponent {
 
+  @Input()
+  servicos: Servico[] = [
+    {
+      //crie com base no modelo Servico
+      id: 1,
+      nome: 'Corte de Cabelo',
+      descricao: 'Corte de cabelo masculino ou feminino, com estilo moderno.',
+      duracaoEmMinutos: 30,
+      nomeProfissional: 'João Silva'
+    },
+    {
+      id: 2,
+      nome: 'Manicure',
+      descricao: 'Serviço de manicure completo, incluindo esmaltação e cuidados com as unhas.',
+      duracaoEmMinutos: 45,
+      nomeProfissional: 'Maria Oliveira'
+    },
+    {
+      id: 3,
+      nome: 'Massagem Relaxante',
+      descricao: 'Massagem relaxante para aliviar o estresse e promover o bem-estar.',
+      duracaoEmMinutos: 60,
+      nomeProfissional: 'Carlos Pereira'
+    }
+  ];
 }
--- src/app/pages/dash-cliente/dash-cliente.component.ts
+++ src/app/pages/dash-cliente/dash-cliente.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { CardComponent } from "../../components/card/card.component";
 
 @Component({
   selector: 'app-dash-cliente',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CardComponent],
   templateUrl: './dash-cliente.component.html',
   styleUrl: './dash-cliente.component.css'
 })
