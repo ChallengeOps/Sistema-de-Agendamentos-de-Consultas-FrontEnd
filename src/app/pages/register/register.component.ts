1cb4ee92ecbd9a423df797582cd3a2e82d1555a5
bootcamp04 <bootcamp04@deloitte.com>
2025-06-03 16:16:36 -0300

    criando registro integrado com backend

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,25 +1,50 @@
 import { Component } from '@angular/core';
-import { Form, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Form, FormControl, FormGroup, FormsModule, NgModel, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { LOGINService } from '../../services/login.service';
+import { signup } from '../../model/signup';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-    userType: 'cliente' | 'profissional' = 'cliente';
     registerForm!: FormGroup;
 
-    constructor(private toastService: ToastrService, private router: Router) {
-        this.registerForm = new FormGroup({
-            name: new FormControl('', [Validators.required]),
-            email: new FormControl('', [Validators.required, Validators.email]),
-            password: new FormControl('', [Validators.required, Validators.minLength(6)])}
-          )
+    constructor(private toastService: ToastrService, private router: Router, private loginservice: LOGINService) {
+       this.registerForm = new FormGroup({
+  name: new FormControl('', Validators.required),
+  email: new FormControl('', [Validators.required, Validators.email]),
+  password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  acesso: new FormControl('CLIENTE') // valor padrão
+});
     }
+
+    signup() {this.loginservice.signup(
+              this.registerForm.value.name,
+              this.registerForm.value.email,
+              this.registerForm.value.password,
+              this.registerForm.value.acesso
+    )?.subscribe({
+  next: () => {
+    this.toastService.success('Cadastro realizado com sucesso!');
+    this.router.navigate(['/']);
+  },
+  error: (err) => {
+    if (err.status === 400 && err.error?.errors) {
+      err.error.errors.forEach((msg: string) => this.toastService.error(msg));
+    } else {
+      this.toastService.error('Erro inesperado ao cadastrar.');
+    }
+  }
+});
+
+}
+
+    
 
     navigate() {
         this.router.navigate(['/login']);
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { ResponseDTO } from '../model/respondedDTO';
+import { signup } from '../model/signup';
 
 @Injectable({
   providedIn: 'root'
@@ -17,4 +18,28 @@ export class LOGINService {
    login(email: string, password: string) {
   return this.httpClient.post<ResponseDTO>(`${this.apiUrl}/login`, { email, password }, { observe: 'response' });
 }
+
+ signup(name: string, email: string, password: string, acesso: string) {
+  const payload = { name, email, password };
+
+  if (acesso === 'PROFISSIONAL') {
+    return this.httpClient.post<ResponseDTO>(
+      `${this.apiUrl}/registerProfissional`,
+      payload,
+      { observe: 'response' }
+    );
+  }
+
+  if (acesso === 'CLIENTE') {
+    return this.httpClient.post<ResponseDTO>(
+      `${this.apiUrl}/register`,
+      payload,
+      { observe: 'response' }
+    );
+  }
+
+  return null;
+}
+
+
 }
